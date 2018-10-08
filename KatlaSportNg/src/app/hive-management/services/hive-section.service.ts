1902b20547be18f1908e6e1ac71d2b5e34950bec
Ivan Shaveka <shaveko.ivan@gmail.com>
2018-10-08 18:52:56 +0300

    Add methods addHiveSection, updateHiveSection and deleteHiveSEction. Add buttons Back, Save, Delete, Undelete and Purge with implementation.

--- KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
+++ KatlaSportNg/src/app/hive-management/services/hive-section.service.ts
@@ -24,4 +24,14 @@ export class HiveSectionService {
   setHiveSectionStatus(hiveSectionId: number, deletedStatus: boolean): Observable<Object> {
     return this.http.put<Object>(`${this.url}${hiveSectionId}/status/${deletedStatus}`, {});;
   }
+
+  addHiveSection(hiveSection: HiveSection): Observable<HiveSection> {
+    return this.http.post<HiveSection>(this.url, hiveSection);
+  }
+   updateHiveSection(hiveSection: HiveSection): Observable<HiveSection> {
+    return this.http.put<HiveSection>(`${this.url}${hiveSection.id}`, hiveSection);
+  }
+   deleteHiveSection(hiveSection: HiveSection): Observable<Object> {
+    return this.http.delete<Object>(`${this.url}${hiveSection.id}`);
+  }
 }
